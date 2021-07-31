a701b60f8391ba17f095bbfd60cb3ff2171fa892
galaeda <caleb.dederick@hotmail.com>
2021-07-31 01:12:12 -0700

    Upgrade to Java 16, modify .gitignore, and add unit testing

---
+++ src/test/java/org/project/cipher/utility/CipherTest.java
@@ -0,0 +1,31 @@
+package org.project.cipher.utility;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class CipherTest {
+
+	Cipher cipher;
+	@BeforeEach
+	void setUp() {
+		cipher = new Cipher();
+	}
+
+	@Test
+	@DisplayName("Encrypt uppercase text")
+	void do_EncodeUppercase() {
+		assertEquals("FPFVDVA", cipher.encode("ENCRYPT"));
+	}
+
+	@Test
+	@DisplayName("Encrypt lowercase text")
+	void do_EncodeLowercase() {
+		assertEquals("fpfvdva", cipher.encode("encrypt"));
+	}
+}
